88c8b16a08ff98283f0339f6bd430dbab851868d
bbirol21 <bbirol21@ku.edu.tr>
2024-09-16 14:34:18 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Hello World Bartu github"
+    #return render_template('index.html')
+
+if __name__ == "_main_":
+    #app.run(host='0.0.0.0', port=5000)
+    app.run(debug = True)
+
+#during the production you should set the debug to False
